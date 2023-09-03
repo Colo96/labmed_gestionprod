7cdcf633761453a6317c8dd92e6af4d9530ba283
Axel Langerman <solomonsilversmith@gmail.com>
2023-09-03 16:25:54 -0300

    Guardando cambios...

--- reactfront/src/components/NavListDrawer.jsx
+++
@@ -1,3 +0,0 @@
-export default function NavListDrawer() {
-  return <></>;
-}
--- reactfront/src/components/TableProducts.jsx
+++ reactfront/src/components/TableProducts.jsx
@@ -6,53 +6,80 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const getURL = "http://localhost:4000/api/products";
 
 export default function TableProducts() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const result = await axios.get(getURL);
+    setPost(result.data);
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Codigo</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Precio</TableCell>
+            <TableCell>Stock</TableCell>
+            <TableCell>Categoria</TableCell>
+            <TableCell>Fecha Alta</TableCell>
+            <TableCell>Borrar</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
+          {post ? (
+            post.length === 0 ? (
+              <TableRow
+                key="codigo"
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  Empty
+                </TableCell>
+                <TableCell>Empty</TableCell>
+                <TableCell>Empty</TableCell>
+                <TableCell>Empty</TableCell>
+                <TableCell>Empty</TableCell>
+                <TableCell>Empty</TableCell>
+              </TableRow>
+            ) : (
+              post.map((product) => (
+                <TableRow
+                  key={product.codigo}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {product.codigo}
+                  </TableCell>
+                  <TableCell>{product.nombre}</TableCell>
+                  <TableCell>{product.precio}</TableCell>
+                  <TableCell>{product.stock}</TableCell>
+                  <TableCell>{product.id_categoria}</TableCell>
+                  <TableCell>{product.fecha_alta}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      startIcon={<DeleteIcon />}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))
+            )
+          ) : null}
         </TableBody>
       </Table>
     </TableContainer>
